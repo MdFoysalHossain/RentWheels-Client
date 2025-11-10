ec957c647fffffa8fba10e6b054a2a50c1a9526c
MdFoysalHossain <imfoysl@gmail.com>
2025-11-10 18:23:14 +0600

    Testimonial Section added to Home Page

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from './Banner';
 import CarFeature from './CarFeature';
 import WhyUs from './WhyUs';
 import Steps from './Steps';
+import Testimonial from './Testimonial';
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <CarFeature/>
             <WhyUs/>
             <Steps/>
+            <Testimonial/>
         </div>
     );
 };
---
+++ src/Components/Pages/Home/Testimonial.jsx
@@ -0,0 +1,123 @@
+
+import React from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { Star } from 'lucide-react';
+import "./Testimonial.css"
+
+const NextArrow = ({ onClick }) => (
+    <button
+        className="absolute z-10 right-5 bottom-12 -translate-y-1/2  p-2 rounded bg-primary text-white hover:bg-white hover:border-primary hover:border hover:text-primary cursor-pointer transition"
+        onClick={onClick}
+    >
+        <ChevronRight size={24} />
+    </button>
+);
+
+const PrevArrow = ({ onClick }) => (
+    <button
+        className="absolute z-10 left-5 bottom-12 -translate-y-1/2  bg-primary text-white p-2 rounded hover:bg-white hover:border hover:text-primary cursor-pointer transition"
+        onClick={onClick}
+    >
+        <ChevronLeft size={24} />
+    </button>
+);
+
+const Testimonial = () => {
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: true,
+    };
+
+
+
+
+    return (
+        <div className="mt-20">
+            <h2 className='text-2xl font-bold text-left mb-10 flex justify-center items-center gap-2 '>Customer <span className='text-primary'>Testimonials</span></h2>
+            <div className="relative max-h-[350px] w-full rounded-xl overflow-hidden ">
+                <Slider {...settings}>
+                    <div className=" flex justify-center rounded-xl bg-white shadow-md items-center h-[400px] w-[500px] overflow-hidden">
+                        <div className="">
+                            <div className="p-4 flex text-left  items-start gap-3">
+                                <img className='w-[100px] h-[100px] rounded-2xl' src="https://static-cse.canva.com/blob/2196696/1600w-B-cRyoh7b98.jpg" alt="" />
+                                <div className="">
+                                    <span className='text-sm'>Name</span>
+                                    <p className='text-lg font-semibold'>Faria Akter</p>
+                                    <span className='text-sm'>Email</span>
+                                    <p className='text-lg font-semibold'>faria.akter@gmail.com</p>
+                                </div>
+                                <div className="text-right w-full h-full">
+                                    <span className='text-primary flex justify-end gap-1'><Star /> <Star /> <Star /> <Star /> </span>
+                                </div>
+                            </div>
+                            <div className="text-left p-4 -mt-[20px]">
+                                <h2 className='text-lg underline underline-offset-6 decoration-primary font-semibold mb-2'>Testimony</h2>
+                                <p className='italic'>"RentWheels made renting a car so easy and convenient! The booking process was quick, and the car was in great condition. I just wish there were a few more pickup locations near my area, but overall a very smooth experience. Definitely recommend!"</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className=" flex justify-center rounded-xl bg-white shadow-md items-center h-[400px] w-[500px] overflow-hidden">
+                        <div className="">
+                            <div className="p-4 flex text-left  items-start gap-3">
+                                <img className='w-[100px] h-[100px] rounded-2xl' src="https://media.istockphoto.com/id/1682296067/photo/happy-studio-portrait-or-professional-man-real-estate-agent-or-asian-businessman-smile-for.jpg?s=612x612&w=0&k=20&c=9zbG2-9fl741fbTWw5fNgcEEe4ll-JegrGlQQ6m54rg=" alt="" />
+                                <div className="">
+                                    <span className='text-sm'>Name</span>
+                                    <p className='text-lg font-semibold'>Rafiq Ahmed</p>
+                                    <span className='text-sm'>Email</span>
+                                    <p className='text-lg font-semibold'>rafiq.ahmed@gmail.com</p>
+                                </div>
+                                <div className="text-right w-full h-full">
+                                    <span className='text-primary flex justify-end gap-1'><Star /> <Star /> <Star /> <Star /> <Star /> </span>
+                                </div>
+                            </div>
+                            <div className="text-left p-4 -mt-[20px]">
+                                <h2 className='text-lg underline underline-offset-6 decoration-primary font-semibold mb-2'>Testimony</h2>
+                                <p className='italic'>"Great platform for renting cars. I loved the variety of cars available. Only minor issue was a slight delay in pickup, but overall a very good experience."</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div className=" flex justify-center rounded-xl bg-white shadow-md items-center h-[400px] w-[500px] overflow-hidden">
+                        <div className="">
+                            <div className="p-4 flex text-left  items-start gap-3">
+                                <img className='w-[100px] h-[100px] rounded-2xl' src="https://media.istockphoto.com/id/1852295642/photo/close-up-portrait-of-pretty-smiling-happy-woman-with-short-hair-in-casual-wear-looking-calmly.jpg?s=612x612&w=0&k=20&c=mlg72Ja7AgoxzPe0M8LCAVwtACLjHKOdG5JjkUq1yXU=" alt="" />
+                                <div className="">
+                                    <span className='text-sm'>Name</span>
+                                    <p className='text-lg font-semibold'>Nusrat Jaman</p>
+                                    <span className='text-sm'>Email</span>
+                                    <p className='text-lg font-semibold'>nusratjaman@gmail.com</p>
+                                </div>
+                                <div className="text-right w-full h-full">
+                                    <span className='text-primary flex justify-end gap-1'><Star /> <Star /> <Star /> <Star /> </span>
+                                </div>
+                            </div>
+                            <div className="text-left p-4 -mt-[20px]">
+                                <h2 className='text-lg underline underline-offset-6 decoration-primary font-semibold mb-2'>Testimony</h2>
+                                <p className='italic'>"RAmazing service! The booking was instant, customer support was very helpful, and the car exceeded my expectations. Highly recommend RentWheels!"</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </Slider>
+           </div>
+        </div>
+    );
+};
+
+export default Testimonial;
